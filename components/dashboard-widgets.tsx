a7a029f21e9fd614ffb281bafa1d60233262a898
Tom de Visser <tomdevisser29@gmail.com>
2025-05-02 18:53:30 +0200

    Added a lot of dashboard components, and the website table

---
+++ components/dashboard-widgets.tsx
@@ -0,0 +1,75 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TrendingUp, TrendingDown } from "lucide-react";
+
+export default function DashboardWidgets() {
+  return (
+    <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+      <Card className="@container/card">
+        <CardHeader className="relative">
+          <CardDescription>Security risks</CardDescription>
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            39
+          </CardTitle>
+          <div className="absolute right-4 top-4">
+            <Badge variant="success" className="flex gap-1 rounded-lg text-xs">
+              <TrendingDown className="size-3" />
+              -12.5%
+            </Badge>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1 text-sm">
+          <div className="text-muted-foreground">
+            This month there were less security risks.
+          </div>
+        </CardFooter>
+      </Card>
+
+      <Card className="@container/card">
+        <CardHeader className="relative">
+          <CardDescription>Performance monitoring</CardDescription>
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            213
+          </CardTitle>
+          <div className="absolute right-4 top-4">
+            <Badge variant="success" className="flex gap-1 rounded-lg text-xs">
+              <TrendingUp className="size-3" />
+              +17
+            </Badge>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1 text-sm">
+          <div className="text-muted-foreground">
+            This translates to an €850 increase in monthly revenue.
+          </div>
+        </CardFooter>
+      </Card>
+
+      <Card className="@container/card">
+        <CardHeader className="relative">
+          <CardDescription>Amount of websites</CardDescription>
+          <CardTitle className="@[250px]/card:text-3xl text-2xl font-semibold tabular-nums">
+            632
+          </CardTitle>
+          <div className="absolute right-4 top-4">
+            <Badge variant="failure" className="flex gap-1 rounded-lg text-xs">
+              <TrendingDown className="size-3" />
+              -3
+            </Badge>
+          </div>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1 text-sm">
+          <div className="text-muted-foreground">
+            We have 3 less sites this month.
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
---
+++ components/website-row.tsx
@@ -0,0 +1,68 @@
+import { TableCell, TableRow } from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+import { TrendingUp, TrendingDown } from "lucide-react";
+
+type Website = {
+  url: string;
+  badges: string[];
+  mainwpBadges: string[];
+  php: string;
+  lastOptimization: string;
+  performance: number;
+  lastPerformance: number;
+  security: string;
+};
+
+interface WebsiteRowProps {
+  website: Website;
+}
+
+export default function WebsiteRow({ website }: WebsiteRowProps) {
+  return (
+    <TableRow>
+      <TableCell className="font-medium">
+        <div className="flex gap-2">
+          {website.url}
+          {website.badges.map((badge, index) => {
+            return (
+              <Badge variant="default" key={index}>
+                {badge}
+              </Badge>
+            );
+          })}
+        </div>
+      </TableCell>
+      <TableCell>
+        <div className="flex gap-2">
+          {website.mainwpBadges.map((badge, index) => {
+            return (
+              <Badge variant="secondary" key={index}>
+                {badge}
+              </Badge>
+            );
+          })}
+        </div>
+      </TableCell>
+      <TableCell>{website.php}</TableCell>
+      <TableCell>{website.lastOptimization}</TableCell>
+      <TableCell className="flex gap-2 items-center">
+        {website.performance}
+        <Badge
+          variant={
+            website.performance - website.lastPerformance > 0
+              ? "success"
+              : "failure"
+          }
+        >
+          {website.performance - website.lastPerformance}
+          {website.performance - website.lastPerformance > 0 ? (
+            <TrendingUp />
+          ) : (
+            <TrendingDown />
+          )}
+        </Badge>
+      </TableCell>
+      <TableCell>{website.security}</TableCell>
+    </TableRow>
+  );
+}
